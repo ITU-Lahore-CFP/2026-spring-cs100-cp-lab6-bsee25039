d6adfc96b753d516edadf20cb6abeb4caed9b96e
bsee25039 <bsee25039@itu.edu.pk>
2026-03-30 18:36:08 +0500

    lab 6  commit

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,21 @@
 
 int sumOfSquares(int n) 
 {
-}
 
+    if (n <= 1)// Base case: if n is 0 or 1, return n squared 
+    {
+        return n * n;// Base case: if n is 0 or 1, return n squared
+    }
+    return (n * n) + sumOfSquares(n - 1);// Recursive call to calculate the sum of squares
+}
+// Task B: Recursive Factorial Function
 int findFactorial(int n)
 {
+if (n <= 1)// Base case: if n is 0 or 1, return 1
+ {
+        return 1;     // Base case: if n is 0 or 1, return 1    
+    }
+    return n * findFactorial(n - 1);// Recursive call to calculate the factorial
 }
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 
 
 int main() {
+    int choice;
+    int n;
+    cout <<"Menu : \n1. Sum of squares\n2. Factorial\n";// Display the menu options
+    cout<<"\nEnter your choice : ";// Prompt the user to enter their choice
+    cin>>choice;        // Read the user's choice
+    switch (choice)// Use a switch statement to handle the user's choice
+    {
+    case (1):   // If the user chooses option 1, calculate the sum of squares
+
+    // Task A: Sum of Squares 
+    cout << "\nEnter a number: "; // Prompt the user to enter a number
+    cin >> n;// Read the number from the user
+    cout << "\nSum of squares: " << sumOfSquares(n) ;// Call the sumOfSquares function and display the result
+        break;
+    case (2):// If the user chooses option 2, calculate the factorial
+    // Task B: Factorial
+    cout << "\nEnter a number: ";// Prompt the user to enter a number
+    cin >> n;// Read the number from the user
+    cout << "\nFactorial of the number is " << findFactorial(n) ;// Call the findFactorial function and display the result
+        break;
+    default:
+       cout<<"\nInvalid choice! Please enter 1 or 2 only."<<endl;// Handle invalid choices by displaying an error message
+        break;
+    }
    
 }
